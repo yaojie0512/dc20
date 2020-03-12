f301cc022c2a25315ec22676f076289b8b14e1d8
yaojie0512 <55313001+yaojie0512@users.noreply.github.com>
2020-03-12 18:09:24 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,335 @@
+﻿<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+<title>典藏20-典藏你的2020</title>
+<meta name="Keywords" content="姚杰，姚杰设计，dc20，典藏20，典藏2020，典藏20世纪，慧校科技，慧校life，慧校LIFE">
+<meta name="description" content="慧校科技旗下典藏20，提供设计干货及资源（非商业用途），站内所有收集的资源都能免费下载，且资源都经过测试后再发布，保证资源无毒无害，大家可放心下载，本站所有资源仅供学习交流">
+<link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/assets/images/favicon.ico" type="images/x-icon" />
+ <link rel="apple-touch-icon" href="img/touch-icon-iphone.png">
+ <link rel="apple-touch-icon" sizes="76x76" href="img/touch-icon-ipad.png">
+ <link rel="apple-touch-icon" sizes="120x120" href="img/touch-icon-iphone-retina.png">
+ <link rel="apple-touch-icon" sizes="152x152" href="img/touch-icon-ipad-retina.png">
+ <link rel="apple-touch-icon" href="apple-touch-icon.png"/>
+<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/style.css">
+<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/assets/css/global_v6.css">
+<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/assets/css/index-title.css">
+<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/assets/css/iconfont.css">
+<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/css/layerslider.css" type="text/css">
+
+<script src="<?php bloginfo('template_url'); ?>/assets/js/jquery.min.js"></script>
+<script src="<?php bloginfo('template_url'); ?>/assets/js/jquery.easing.1.3.js"></script>
+<script src="<?php bloginfo('template_url'); ?>/assets/js/slides.min.jquery.js"></script>
+<script src="<?php bloginfo('template_url'); ?>/assets/js/qshare_min.js"></script>
+
+<script src="<?php bloginfo('template_url'); ?>/assets/js/jquery.js" type="text/javascript"></script>
+<script src="<?php bloginfo('template_url'); ?>/assets/js/greensock.js" type="text/javascript"></script>
+<script src="<?php bloginfo('template_url'); ?>/assets/js/common.js" type="text/javascript"></script>
+<script src="<?php bloginfo('template_url'); ?>/assets/js/layerslider.kreaturamedia.jquery.js" type="text/javascript"></script>
+<script type="text/javascript" charset="utf-8" src="<?php bloginfo('template_url'); ?>/assets/js/page_context.js"></script>
+<link href="<?php bloginfo('template_url'); ?>/assets/css/media-queries.css" rel="stylesheet" type="text/css">
+  
+<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/css/master.css">
+<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/assets/js/pace.min.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/assets/js/script.js"></script>
+<div id="pointer"></div>
+<style>body {margin: 0;}canvas {cursor: crosshair;display: block;position: absolute;z-index: 999;}#fireworks-close {position: fixed;right: 0px;z-index: 9999;}</style>
+<link rel="manifest" href="js/manifest.json">
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="apple-mobile-web-app-status-bar-style" content="black">
+  <meta name="apple-mobile-web-app-title" content="DC20">
+ <!-- <link rel="apple-touch-icon" href="img/icon-152x152.png">-->
+  <meta name="msapplication-TileImage" content="img/icon-144x144.png">
+  <meta name="msapplication-TileColor" content="#2F3BA2">
+</head>
+ <body id = 'content-warp'>
+  <script type="text/javascript">window.onload=getCSS();</script>
+    <div id="pagewrap">
+	<div id="page">
+        <!--
+         谁还没个梦想
+         愿世间多些善良
+         少些疾苦
+         --Jwine
+        -->
+
+<div id="header">
+<header>
+  <div class="container clearfix">
+    <h1 id="logobag">首页</h1>
+    <li style="  width: 40px;">
+      <a id="xinlang" href="<?php bloginfo('template_url');?>/message.php" target="_blank" title="赞助">
+      <i class="icon iconfont icon-liuyan"></i></a>
+    </li>
+        <li class="search bar8" style="margin-right: 20px;">
+         <form id="search_form">
+             <div class="bar8" style="margin-top: 6px;">
+              <i class="icon iconfont icon-sousuo" style="font-size: 24px;color: #9ea2a9;"></i>
+             <input id="bdcsMain"  type="text" name="lname"  placeholder="在这搜索呗" >
+             </div>
+        </form>
+    </li>
+
+<script>
+$( "#search_form" ).submit(function( event ) {
+  window.open('http://zhannei.baidu.com/cse/search?s=15272378007431883036&entry=1&q='+document.getElementById('bdcsMain').value)
+  event.preventDefault();
+});
+$( "#search_form_mobile" ).submit(function( event ) {
+  window.open('http://zhannei.baidu.com/cse/search?s=15272378007431883036&entry=1&q='+document.getElementById('bdcsMain-mobile').value)
+  event.preventDefault();
+});
+$("[name='search_btn']").click(function(event) {
+  window.open('http://zhannei.baidu.com/cse/search?s=15272378007431883036&entry=1&q='+document.getElementById('bdcsMain').value)
+});
+</script>
+
+  </div>
+</header>
+<!-- /header -->
+
+            <!--[if !IE]><!-->
+      <div class="top">
+        <div id="dd" class="wrapper-dropdown-5" tabindex="1" style="padding: 0px !important;">
+          <img src="<?php bloginfo('template_directory'); ?>/assets/images/nav_icon.png" width="44" height="44">
+          <ul class="dropdown">
+            <div class="index_nav3" style="padding: 0px 0px 0px 9px !important;">
+              <div class="index_nav_icon" style="padding: 0px !important;">
+                <a href="#">典藏20</a></div>
+              <div class="index_nav_icon" style="padding: 0px !important;">
+                  <!--  <a href="#">关于我</a></div>
+                  <div class="index_nav_icon" style="padding: 0px !important;">
+                    <a href="#">Pencil</a></div>
+                  <div class="index_nav_icon" style="padding: 0px !important;">
+                    <a href="#">记录线</a></div>
+                  <div class="index_nav_icon" style="padding: 0px !important;">
+                    <a href="#">工具箱</a></div>
+                  <div class="index_nav_icon" style="padding: 0px !important;">
+                    <a href="#">赞助</a></div>-->
+            </div>
+          </ul>
+        </div>
+        <div class="logo" style="padding: 0px !important;">
+            <img src="<?php bloginfo('template_directory'); ?>/assets/images/logo.png" width="44" height="44">
+        </div>
+      <div id="pencil" tabindex="1" style="padding: 0px !important;">
+          <a href="down/message.html"><img src="<?php bloginfo('template_directory'); ?>/assets/images/pencil_icon.png" width="44" height="44"></a>
+      </div>
+       </div>
+        </div>
+    <!--<![endif]-->
+<!--banner-->
+<div id="slider-wrapper">
+		<div id="layerslider" class="ls-wp-container" style="width:1349px;height:591px;">
+
+   <!--        <div class="ls-slide" data-ls="slidedelay:8000;transition3d:all;">
+      <a href="#"  class="ls-link" target="_blank"><img src="assets/images/20181213.jpg" class="ls-bg" alt="3D STAR - 在线制作3D建筑"/></a>
+      </div> -->
+
+            <div class="ls-slide" data-ls="slidedelay:6666;transition3d:all;">
+                <a   class="ls-link" target="_blank"><img src="<?php bloginfo('template_directory'); ?>/assets/images/lanbor.png" class="ls-bg" alt="兰博基尼"/></a>
+            </div>
+            <div class="ls-slide" data-ls="slidedelay:6666;transition3d:all;">
+                <a href="#"  class="ls-link" target="_blank"><img src="<?php bloginfo('template_directory'); ?>/assets/images/201811140001.jpg" class="ls-bg" alt="分享捷径-我们一起创造"/></a>
+            </div>
+
+<!--           <div class="ls-slide" data-ls="slidedelay:8000;transition3d:all;">
+      <a href="#"  class="ls-link" target="_blank"><img src="assets/images/3moshed-2018-8-22-12-19-43.gif" class="ls-bg" alt="一键生成故障艺术"/></a>
+      </div> -->
+
+
+		  <div class="ls-slide" data-ls="slidedelay:6666;transition3d:all;">
+			<a href="#"  class="ls-link" target="_blank"><img src="<?php bloginfo('template_directory'); ?>/assets/images/2018011701.jpg" class="ls-bg" alt="Kyle T. Webster的独家画笔"/></a>
+			</div>
+
+
+        </div>
+</div>
+
+
+	<!--功能模块-->
+<div class="central_k">
+<div class="central"> 
+    <div class="design_studio" id="border1" ><a href="#" target="_blank" ><i class="icon iconfont icon-Group" id="icon"></i><br><font face="微软雅黑" >时 光 机</font></a></div>
+<!--     <div class="design_studio" id="border2" ><a href="#" target="_blank" ><i class="icon iconfont icon-liwuhe" id="icon"></i><br><font face="微软雅黑" >衍 预 约</font></a></div> -->
+    <div class="design_studio" id="border3"><a href="#" target="_blank" ><i class="icon iconfont icon-gjux" id="icon"></i><br><font face="微软雅黑" >工 具 箱</font></a></div>
+    <div class="design_studio" id="border2" ><a href="#" target="_blank" ><i class="icon iconfont icon-liwuhe" id="icon"></i><br><font face="微软雅黑" >调 色 板</font></a></div>
+
+    <!--  <div class="design_studio" id="border4"><a href="#" target="_blank" ><i class="icon iconfont icon-GroupCopy6" id="icon"></i><br><font face="微软雅黑" >爱 捷 径</font></a></div>
+     <div class="design_studio" id="border5"><a href="#" target="_blank" ><i class="icon iconfont icon-Group4" id="icon"></i><br><font face="微软雅黑" >S k e t c h</font></a></div>
+     <div class="design_studio" id="border6"><a href="#" target="_blank" ><i class="icon iconfont icon-Group5" id="icon"></i><br><font face="微软雅黑" >资 源 库</font></a></div>
+ </div>
+ </div>-->
+
+             <!--通用底部信息-->
+  <div id="sidebar">
+		<div class="copyrights">
+  <div class="copyrights_z">
+        <dl>
+        <dd class="copyrights_wz" style="padding-right: 84px;">导航<br>
+        <a href="#" target="_blank" title="dc20">典藏20</a>　
+     <!--   <a href="#" target="_blank" title="记录">记录线</a>　
+        <a href="#" target="_blank" title="工具箱">工具箱</a>　
+        <a href="#" target="_blank" title="简历">. &nbsp; &nbsp;</a>
+        <a target="_blank" href="#" >典藏2020</a>-->　
+        <a target="_blank" href="#" style="color: #dca90e;" >qq:2833819698</a></dd>
+        <dd class="copyrights_wz">友情Link<br>
+
+			  <a href="https://www.socc.top/" target="_blank">SOCC资源网</a>　
+              <a href="https:/huixiaolife.cn" target="_blank">慧校LIFE</a>　
+		</dd>
+
+		<dd class="about_logo"><a href="#" title="关于" target="_blank"> <img src="<?php bloginfo('template_directory'); ?>/assets/images/4--logo.png">  </a></dd>
+		<!-- <dd class="about_logo"><a href="About_us.htm" title="关于" target="_blank"><i class="icon iconfont icon-_huaban" style="font-size: 52px;"></i></a></dd>	  -->
+        </dl>
+  </div>
+  </div>
+<div class="copyrights_c">
+<div class="m_icon"><a href="#" target="_blank" title="微信公众号"><i class="icon iconfont icon-wechat-copy" style="font-size:18px;float:  left;margin-top:-3px;" ></i>微信公众号</a></div>
+<div class="copyrights_certificate"><a href="http://www.beian.miit.gov.cn/" target="_blank">赣ICP备19011016号-3</a>　<a>典藏你的2020</a>　Copyright © dc20.top. All Rights Reserved.</div>
+
+</div>
+</div>
+</div>
+		<!--[if !IE]><!-->
+			<div class="cb" style="font-size: 12px;float: right; margin-top: -50px;margin-right: 35px;color: #A2A2A2;">
+			<p><a href="http://www.miibeian.gov.cn/" target="_blank" style="color: #ababab;">赣ICP备19011016号-3</a><a href="http://dc20.top" target="_blank" style="color: #ababab;">dc20.top</a>版权所有.</p>
+		    </div>
+		<!--<![endif]-->
+		<!--[if !IE]><!-->
+		<div class="copyright">
+			<p style="margin: 0 0 0.2em;width:70%; padding-right: 10px;padding-left: 12px;">
+				<a href="#">版权所有</a>
+			</p>
+			<p style="width: 280px;padding-left: 12px;">
+				Copyright ©dc20.top. 版权所有.
+			</p>
+		</div>
+		<!--<![endif]-->
+	</div>
+	</div>
+
+	<!--<script src="assets/js/baiduseorandom.js"></script>-->
+
+<!--2d效果 -->
+<!--
+
+<script>
+		var x1 = document.getElementsByClassName("ls-slide")[0].offsetLeft;
+		var y1 = document.getElementsByClassName("ls-slide")[0].offsetTop;
+		var x2;
+		var index = 0;
+
+		window.onload = function() {
+			timer = setInterval(auto, 3000);
+		}
+
+		function auto() {
+			index++;
+			var q=index%2
+			if(q==0){
+				$(".ls-active").fadeIn();
+			}
+
+		}
+
+		function show() {
+			$(".tu1").attr("src", imgarr[index][0])
+		}
+
+		$(".ls-slide").mousemove(function(e) {
+			check(e);
+
+		})
+
+		function check(e) {
+			var a = e ? e : window.event;
+			x2 = e.offsetLeft - x1;
+			y2 = e.clientY - y1;
+			var LorR = x2 - 610;
+			var RorL = y2 - 250;
+			var x3 = x1 + 610 - e.clientX;
+			var y3 = y1 + 250 - e.clientY;
+			if(LorR >= 0) {
+				if(RorL >= 0) {
+					$(".ls-slide").css("transform", "perspective(3000px)rotateX(" + y2 / 600 * 8 + "deg)rotateY(" + -(-x2 / 1370 * 8) + "deg)");
+
+				}
+
+			} else {
+				$(".ls-slide").css("transform", "perspective(3000px)rotateX(" + (y3 / 600 * 8) + "deg)rotateY(" + -x3 / 1370 * 8 + "deg)");
+			
+			}
+
+		}
+		//手机端效果
+//		var phoneX,phoneY;
+//		
+//		$(".ls-slide").ontouchstart(function(){
+//			phoneX=event.pageX;
+//			phoneY=event.pageY;
+//		})
+//		$(document).ontouchmove(DocumentTouch(){
+//			var x= event.pageX-phoneX;
+//			var y= event.pageY-phoneY;
+//				$(".ls-slide").css("transform", "perspective(3000px)rotateX(" + (y / 600 * 8) + "deg)rotateY(" + -x / 1370 * 8 + "deg)");
+//		})
+	</script>-->
+
+
+<script>
+//手机打开浮层
+$("#dd").click(function(){
+	//var s = $(".dropdown").css('overflow');
+	/*if ( s=='hidden' ){
+		$(".dropdown").css('overflow','visible');
+	}else{
+		$(".dropdown").css('overflow','hidden');
+	}*/
+	var s = $(this).hasClass('active');
+	if ( s ){
+		$(this).removeClass('active');
+	}else{
+		$(this).addClass('active');
+	}
+	var b = $(".search_top").css('display');
+
+	if ( b=='block'  ){$(".search_top").css('display','none');
+}
+})
+$(".so_icon").click(function(){
+	var s = $(".search_top").css('display');
+	if ( s=='none' ){
+		$(".search_top").css('display','block');
+	}else{
+		$(".search_top").css('display','none');
+	}
+	var b = $("#dd").hasClass('active');
+	if ( b  ){$("#dd").removeClass('active');
+	}
+})
+</script>
+	<!-- Initializing the slider -->
+	<script>
+		jQuery("#layerslider").layerSlider({
+			pauseOnHover: false,
+			skin: 'borderlessdark3d',
+			hoverBottomNav: true,
+			skinsPath: './layerslider/skins/'
+		});
+	</script>
+
+	<!--<script src="assets/js/fireworks.js"></script>
+	<script>
+		$('#fireworks-close').click(function(){
+			$('.fireworks-body').hide()
+		})
+	</script>-->
+
+	
+	<div style = "clear: both"></div>
+</body>
+</html>
+
